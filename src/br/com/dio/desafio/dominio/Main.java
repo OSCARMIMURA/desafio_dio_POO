3ab38eb7528d499887d626de145c8f8baea626c4
oscarmimura <oscar.mimura@gmail.com>
2021-11-29 23:59:40 -0300

    finalização do código

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -15,12 +15,37 @@ public class Main {
         curso2.setCargaHoraria(4);
 
         Mentoria mentoria1 = new Mentoria();
-        mentoria1.setTitulo("Mentoria em Jasa");
+        mentoria1.setTitulo("Mentoria em Java");
         mentoria1.setDescricao("mentoria sobre Java");
         mentoria1.setData(LocalDate.now());
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+        //System.out.println(curso1);
+        //System.out.println(curso2);
+        //System.out.println(mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("BootCamp Java Developer");
+        bootcamp.setDescricao("Descrição sobre Bootcamp");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.increverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devJoao.getConteudosInscritos());
+        System.out.println("Conteudos  Concluídos" + devJoao.getConteudosConcluidos());
+        devJoao.progredir();
+
+        System.out.println("Conteudos Inscritos" + devJoao.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos" + devJoao.getConteudosConcluidos());
+
+
+        Dev devJose = new Dev();
+        devJose.setNome("Jose");
+        devJose.increverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos " + devJose.getConteudosInscritos());
+        System.out.println("Conteudos  Concluídos " + devJose.getConteudosConcluidos());
 
 
 
