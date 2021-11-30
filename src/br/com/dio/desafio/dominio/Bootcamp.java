8d5f64a44e4c06b4d6f0e5df5019e31367bc4e04
oscarmimura <oscar.mimura@gmail.com>
2021-11-30 00:00:53 -0300

    finalização do código

---
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,64 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Bootcamp {
+
+
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+    // hashset porque não pode haver 2 devs iguais
+    private Set<Dev> devsInscricao = new HashSet<>();
+    // linkedHashset porque os cursos tem uma ordem
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public Set<Dev> getDevsInscricao() {
+        return devsInscricao;
+    }
+
+    public void setDevsInscricao(Set<Dev> devsInscricao) {
+        this.devsInscricao = devsInscricao;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+
+
+
+
+}
